45047cf7d04890aded5ab9b11f1d8259cc88cd53
Abhishek-1310 <kumaryadavabhi1999@gmail.com>
2025-06-15 14:50:35 +0530

    done changes in lambda

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 exports.handler = async (event) => {
+    console.log("event", event);
     return {
         statusCode: 200,
         body: JSON.stringify({
             message: "Hello from Lambda!",
-            input: event
         }),
     };
 };
